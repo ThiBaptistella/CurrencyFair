53630a482f7ebeb14f89b3f352c5e928df6ded88
thiago baptistella <thiago_baptistella1@hotmail.com>
2019-05-14 23:41:02 +0100

    add logic

--- src/components/transactionInfo/transactionForm.js
+++ src/components/transactionInfo/transactionForm.js
@@ -5,85 +5,81 @@ import "./transactionInfo.css";
 
 import Modal from '../modal/modal';
 
-const options = [
-  { value: 'euro', label: 'Euro' },
-  { value: 'dollar', label: 'Dollar' }
-];
-
 class TransactionForm extends Component {
 	constructor(props) {
         super(props);
         this.state = {
-            value: "",
+            currency: "",
             youSend: "",
             receiverGets: "",
-            code1: "",
-            code2: "",
-            code3: "",
-            code4: "",
-            code5: "",
-            code6: "",
             selectedOption: null,
             isShowing: false
         }
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleInput = this.handleInput.bind(this);
     } 
-    openModalHandler = () => {
-      this.setState({
-          isShowing: true
-      });
-    }
-    closeModalHandler = () => {
-      this.setState({
-          isShowing: false
-      });
-    }
+    // openModalHandler = () => {
+    //   this.setState({
+    //       isShowing: true
+    //   });
+    // }
+    // closeModalHandler = () => {
+    //   this.setState({
+    //       isShowing: false
+    //   });
+    // }
     handleChange = (e) => {
-      let {name, value} = e.target;
-      this.setState({
-        [name]: value,
-      
-      });
-      console.log(e);
+      this.handleInput({
+        currency: e.target.value
+      })
     }
+       
+    handleInput(e) {
+      this.setState({ ...e }, () => {
+        console.log("this.state", this.state);
         
+      });
+    }
     handleSubmit(e){
         e.preventDefault();
-        this.setState({ [e.target.name]: e.target.value });
-        console.log(e);
+        this.setState({ [e.target.name]: e.target.value }, () => { console.log(this.state.name) })
         //Send state to the server code
+
     }
 
       render() {
-        const { selectedOption } = this.state;
         return (
           <div>
           <form onSubmit={this.handleSubmit}>
             <div className="field">
-                <input type="text" name="youSend" id="youSend" placeholder="$1000" />
-                <label htmlFor="youSend">You Send</label>
+                <input type="text" name="youSend" id="youSend" placeholder="$1000"   onChange={(e) => {
+                  this.handleInput({
+                    currency: e.target.value
+                  })
+                } } value={this.state.currency}/>
+
+              <label htmlFor="youSend">You Send</label>
                 <div className="flag-container">
                   <select 
+                    onChange={this.handleChange}
                     className="flag"
-                    value={this.state.value}
                   >
-                    <option value="euro">Euro</option>
-                    <option value="dollar">Dollar</option>
+                    <option value="£">£</option>
+                    <option value="$">$</option>
                   </select>
                 </div>
             </div>
-                
             <div className="field">
-                <input type="text" name="receiverGets" id="receiverGets" placeholder="£1000" />
+                <input type="text" name="receiverGets" id="receiverGets" placeholder="$1000" onChange={this.handleInput} value={this.state.currency} />
                 <label htmlFor="receiverGets">receiver gets</label>
                 <div className="flag-container">
                   <select 
-                  
+                    onChange={this.handleChange}
                     className="flag"
-                    value={this.state.value}
                   >
-                    <option value="euro">Euro</option>
-                    <option value="dollar">Dollar</option>
+                    <option value="£">£</option>
+                    <option value="$">$</option>
                   </select>
                 </div>
             </div>
@@ -100,7 +96,7 @@ class TransactionForm extends Component {
          { this.state.isShowing ? <div onClick={this.closeModalHandler} className="back-drop"></div> : null }
 
             
-          <Modal
+          {/* <Modal
               show={this.state.isShowing}
               close={this.closeModalHandler}>
                 <div className="code-container">
@@ -118,7 +114,8 @@ class TransactionForm extends Component {
                 </div>
                  
 
-          </Modal></div>
+          </Modal> */}
+          </div>
           
         );
     }
